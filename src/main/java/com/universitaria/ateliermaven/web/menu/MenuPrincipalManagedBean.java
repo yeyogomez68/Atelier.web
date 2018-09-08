60fba5a27266e52aed9b6d957f42688540740600
Jeisson Gomez <Jeisson Gomez@DESKTOP-FV6VQJ9>
2018-09-08 11:33:52 -0500

    Definicion de espacio

--- src/main/java/com/universitaria/ateliermaven/web/menu/MenuPrincipalManagedBean.java
+++ src/main/java/com/universitaria/ateliermaven/web/menu/MenuPrincipalManagedBean.java
@@ -6,12 +6,14 @@
 package com.universitaria.ateliermaven.web.menu;
 
 import com.universitaria.atelier.web.jpa.Opcion;
+import com.universitaria.atelier.web.jpa.Usuario;
 import com.universitaria.ateliermaven.ejb.menu.OpcionEJB;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
 import org.primefaces.model.menu.DefaultMenuItem;
 import org.primefaces.model.menu.DefaultMenuModel;
 import org.primefaces.model.menu.DefaultSubMenu;
@@ -24,7 +26,7 @@ import org.primefaces.model.menu.MenuModel;
 public class MenuPrincipalManagedBean implements Serializable{
     
     private MenuModel model;
-    
+    private Usuario user;
     @EJB
     private OpcionEJB opcionEJB;
     
@@ -32,6 +34,8 @@ public class MenuPrincipalManagedBean implements Serializable{
     private List<Opcion> listOpcionItems;
 
     public MenuPrincipalManagedBean() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        user = (Usuario) facesContext.getExternalContext().getSessionMap().get("user");
     }
     @PostConstruct
     public void init() {
@@ -39,8 +43,8 @@ public class MenuPrincipalManagedBean implements Serializable{
         listOpcionModulos = new ArrayList<>();
         listOpcionItems = new ArrayList<>();
         
-        listOpcionModulos = opcionEJB.getOpcionesByPerm(1,null);
-        listOpcionItems = opcionEJB.getOpcionesByPerm(2,null);
+        listOpcionModulos = opcionEJB.getOpcionesByPerm(1,user.getRollId().getRollId());
+        listOpcionItems = opcionEJB.getOpcionesByPerm(2,user.getRollId().getRollId());
         
         for (Opcion opciModulos : listOpcionModulos) {
             DefaultSubMenu mod = new DefaultSubMenu(opciModulos.getFormulario());            
@@ -57,6 +61,16 @@ public class MenuPrincipalManagedBean implements Serializable{
         }
     }
 
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+
+    
+    
     public MenuModel getModel() {
         return model;
     }
